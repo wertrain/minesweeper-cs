ab9a7b2b65df6d6a7599cc6905f8495fb8a3f308
wertrain <wertrain@gmail.com>
2021-05-06 23:20:05 +0900

    プロジェクトの追加

---
+++ Minesweeper/Minesweeper/MinesweeperCore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// マインスイーパーのコアクラス
+    /// </summary>
+    public class MinesweeperCore
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MinesweeperCore()
+        {
+            Console.WriteLine("Hello, Minesweeper!");
+        }
+    }
+}
---
+++ Minesweeper/MinesweeperConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var core = new Minesweeper.MinesweeperCore();
+        }
+    }
+}
