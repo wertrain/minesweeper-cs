393e8ce373c8b4802751f235d95dc339af151000
wertrain <wertrain@gmail.com>
2021-05-08 10:27:15 +0900

    だいたいの機能を実装

--- Minesweeper/MinesweeperConsole/Program.cs
+++ Minesweeper/MinesweeperConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MinesweeperLib;
+using System.Collections.Generic;
 
 namespace MinesweeperConsole
 {
@@ -7,9 +8,41 @@ namespace MinesweeperConsole
         static void Main(string[] args)
         {
             var config = new Minesweeper.Configuration();
-            config.FieldWidth = 24;
-            config.FieldHeight = 24;
             var minesweeper = Minesweeper.CreateGame(config);
+
+            minesweeper.Open(1, 1);
+
+            var state = new Dictionary<Minesweeper.FieldState, string>
+            {
+                { Minesweeper.FieldState.Unopen, "■" },
+                { Minesweeper.FieldState.OpenedEmptyMine, "□" },
+                { Minesweeper.FieldState.Opened1Mine, "１" },
+                { Minesweeper.FieldState.Opened2Mine, "２" },
+                { Minesweeper.FieldState.Opened3Mine, "３" },
+                { Minesweeper.FieldState.Opened4Mine, "４" },
+                { Minesweeper.FieldState.Opened5Mine, "５" },
+                { Minesweeper.FieldState.Opened6Mine, "６" },
+                { Minesweeper.FieldState.Opened7Mine, "７" },
+                { Minesweeper.FieldState.Opened8Mine, "８" },
+                { Minesweeper.FieldState.OpenedMine, "＠" },
+                { Minesweeper.FieldState.Flag, "▽" },
+            };
+
+            for (int y = 0; y < config.FieldHeight; ++y)
+            {
+                for (int x = 0; x < config.FieldWidth; ++x)
+                {
+                    if (minesweeper.CheckMine(x, y))
+                    {
+                        System.Console.Write("＠");
+                    }
+                    else
+                    {
+                        System.Console.Write(state[minesweeper.GetFieldState(x, y)]);
+                    }
+                }
+                System.Console.WriteLine();
+            }
         }
     }
 }
